2d3da567decddcecf8fee2181a973f8ac2900d5c
abhikantb <abhikantb@gmail.com>
2026-06-20 14:59:43 +0530

    chatbot with streaming and tool display

--- backend/graph.py
+++ backend/graph.py
@@ -11,24 +11,74 @@ from langgraph.graph import StateGraph,START,END
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3
 from langsmith import traceable
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
+import requests
+import os
+weatherstack_API_KEY = os.getenv("weatherstack_API_KEY")
+alpha_vantage_stock_API_KEY = os.getenv("alpha_vantage_stock_API_KEY")
 
+# -------------------
+# 1 tools 
+# -------------------
+search_tool = DuckDuckGoSearchRun(region="us-en")
 
+@tool
+def get_weather_data(city: str) -> str:
+    """
+    This function fetches the current weather data for a given city
+    """
+    url = f'https://api.weatherstack.com/current?access_key={weatherstack_API_KEY}&query={city}'
+    response = requests.get(url)
+    return response.json()
+
+@tool
+def get_stock_price(symbol: str) -> dict:
+    """
+    Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA') 
+    using Alpha Vantage with API key in the URL.
+    """
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={alpha_vantage_stock_API_KEY}"
+    r = requests.get(url)
+    return r.json()
+
+tools = [search_tool, get_weather_data, get_stock_price]
+llm_with_tools = llm.bind_tools(tools)
+
+# -------------------
+# 2 state
+# -------------------
 class ChatState(TypedDict):
     messages : Annotated[list[BaseMessage],add_messages]
+
+# -------------------
+# 3 all nodes
+# -------------------
+tool_node = ToolNode(tools)
 
 @traceable
 def chat_node(state:ChatState):
     messages = state['messages']
-    response= llm.invoke(messages)
+    response= llm_with_tools.invoke(messages)
     return {'messages':[response]}
 
+# -------------------
+# 4 checkpointer
+# -------------------
 conn= sqlite3.connect(database='chatbot.db',check_same_thread=False)
 checkpointer = SqliteSaver(conn=conn)
 
+# -------------------
+# 5 graph
+# -------------------
 graph=StateGraph(ChatState)
 graph.add_node('chat_node',chat_node)
+graph.add_node('tools',tool_node)
+
 graph.add_edge(START,'chat_node')
-graph.add_edge('chat_node',END)
+graph.add_conditional_edges('chat_node',tools_condition)
+graph.add_edge('tools', 'chat_node')
 
 chatbot=graph.compile(checkpointer=checkpointer)
 
@@ -42,3 +92,16 @@ chatbot=graph.compile(checkpointer=checkpointer)
 # for message_chunk, metadata in stream:
 #     if message_chunk.content:
 #         print(message_chunk.content, end=" ", flush=True)
+
+# if __name__ == "__main__":
+#     from langchain_core.messages import HumanMessage
+    
+#     # This sends a message to your compiled chatbot
+#     config = {"configurable": {"thread_id": "test_thread"}}
+#     initial_state = {"messages": [HumanMessage(content="Hello! Can you hear me?")]}
+    
+#     print("Invoking chatbot...")
+#     response = chatbot.invoke(initial_state, config=config)
+    
+#     # Print the last message from the AI
+#     print("AI Response:", response["messages"][-1].content)
--- frontend/helper.py
+++ frontend/helper.py
@@ -1,22 +1,39 @@
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage,ToolMessage
 from backend.graph import chatbot,checkpointer
+import streamlit as st
 
 def get_stream_generator(user_query, thread_id):
     config = {
         "configurable": {"thread_id": thread_id},
-        "metadata": {
-            "thread_id": thread_id
-        },
+        "metadata": {"thread_id": thread_id},
         "run_name": "chat_turn"
     }
-    stream = chatbot.stream(
-        {"messages": [HumanMessage(content=user_query)]},
-        config=config,
-        stream_mode="messages"
-    )
-    for message_chunk, metadata in stream:
-        if message_chunk.content:
-            yield message_chunk.content
+    status_holder = {"box": None} # for displaying tool name in UI
+    def ai_only_stream():
+        stream = chatbot.stream(   # streaming(display) messages as soon as llm generates them
+            {"messages": [HumanMessage(content=user_query)]},
+            config=config,
+            stream_mode="messages",
+        )
+        for message_chunk, metadata in stream:
+            if isinstance(message_chunk, ToolMessage):
+                tool_name = getattr(message_chunk, "name", "tool")
+                if status_holder["box"] is None:
+                    status_holder["box"] = st.status(f"🔧 Using '{tool_name}'...", expanded=True)
+                else:
+                    status_holder["box"].update(label=f"🔧 Using '{tool_name}'...", state="running", expanded=True)
+                continue    # Skip yielding ToolMessages as stream message, go for next message chunk
+                            # otherwise tool msg will be displayed unnecesary in chat window
+            
+            # filter only AI messages to be displayed to the user (HumanMessage and ToolMessage are not displayed)
+            if message_chunk.content and not isinstance(message_chunk, (HumanMessage, ToolMessage)): 
+                # If a tool box is still open, close it when AI text starts
+                if status_holder["box"]:
+                    status_holder["box"].update(label="✅ Tool finished", state="complete", expanded=False)
+                    status_holder["box"] = None
+                yield message_chunk.content
+                
+    return ai_only_stream()
 
 def get_thread_state(thread_id):
     config = {'configurable': {'thread_id': thread_id}}
